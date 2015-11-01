eca3e909ed80073be7e35eb0a41581fef2a979cd
Senthil Kumar Gundepalli <sgundepa@cisco.com>
2015-11-01 22:15:37 +0530

    fix typo mistake

--- features/devopstest.feature
+++ features/devopstest.feature
@@ -2,7 +2,5 @@ Feature: Getting Started With cucumber
 
   Scenario: need to checkout develop branch
     Given the createFeatureBranchFromDevelop Object
-    When the user called the 'checkoutBranch' function with 'devlop' as param
-	  Then The Output of 'getCurrentBranch' name should be develop
-
-  
+    When the user called the 'checkoutBranch' function with 'devlop' as a param
+	  Then The Output of 'getCurrentBranch' function should be develop
--- features/step_definitions/devopstest.step.rb
+++ features/step_definitions/devopstest.step.rb
@@ -4,10 +4,10 @@ Given(/^the createFeatureBranchFromDevelop Object$/) do
   Obj=CreateFeatureBranchFromDevelop.new("C:\\Users\\sgundepa\\Desktop\\Work\\cucumber\\test\\","feature1")
 end
 
-When(/^the user called the 'checkoutBranch' function with 'devlop' as param$/) do
+When(/^the user called the 'checkoutBranch' function with 'devlop' as a param$/) do
   expect(Obj.checkoutBranch("develop")).to be true
 end
 
-Then(/^The Output of 'getCurrentBranch' name should be develop$/) do
+Then(/^The Output of 'getCurrentBranch' function should be develop$/) do
     expect(Obj.getCurrentBranch()).to eq("* develop")
 end
